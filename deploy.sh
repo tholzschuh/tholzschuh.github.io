005e3708e59f4afa492755e06e8de3734fe6fbb7
Tim Holzschuh <timholzschuh@gmx.de>
2021-01-10 16:20:51 +0000

    Revert "Publish."
    This reverts commit 000ec68ae04def0b319bf787052f42d93868095c.

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+git checkout develop
+
+nix-build
+./result/bin/blog build
+
+git add -A
+git commit -m "rebuilt site."
+
+git checkout master
+
+rm -R *
+
+git checkout develop -- _site/
+
+git mv -f _site/* .
+
+rmdir _site/
+
+git add -A
+
+git commit -m "Publish."
+
+git push origin master
+
+git checkout develop
---
+++ maths/2021-01-10-computing-simple-factors-of-certain-jacobian-varieties-over-finite-fields.md
@@ -0,0 +1,8 @@
+---
+title: Computing simple factors of certain Jacobian varieties over finite fields
+author: Tim Holzschuh
+tags: article
+---
+
+My undergraduate thesis.
+
