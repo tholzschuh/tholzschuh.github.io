565001fd5d4c85755ec1213d1f1003a550518c36
Tim Holzschuh <timholzschuh@gmx.de>
2021-01-12 07:02:30 +0000

    rebuilt site.

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,23 @@ nix-build
 git add -A
 git commit -m "rebuilt site."
 
+echo "_site/ develop:"
+ls _site/
+
 git checkout master
+
+echo "master: "
+ls 
 
 rm -Rf ./* 
 
+echo "master after cleaning up:"
+ls -a
+
 git checkout develop --  _site/
+
+echo "_site/ master:"
+ls _site/
 
 git mv -f _site/* .
 
